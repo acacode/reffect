fcdb0059a1379d870444a5684bc04c6d60b443e9
svolkov <svolkov@everpoint.ru>
2020-03-09 12:30:07 +0300

    feat: add logger package; feat: add export types for core + store name

--- packages/logger/index.ts
+++ packages/logger/index.ts
@@ -1,5 +1,19 @@
-// import { StoreManager, Middleware } from "@reffect/core";
+import { Middleware, StoreManager, Watcher } from "@reffect/core";
 
-export const logger = <Store = any>(storeManager: any) => {
-  console.log("s");
+export const logger: Middleware<any> = <Store extends object>(
+  storeManager: StoreManager<Store>
+) => {
+  if (process.env.NODE_ENV === "development") {
+    console.log(
+      `store "${storeManager.name}" initialize`,
+      storeManager.initialState
+    );
+
+    const watcher: Watcher<Store> = partialUpdate => {
+      console.log(`store "${storeManager.name}" update`, partialUpdate);
+    };
+
+    storeManager.watch(watcher);
+  }
+  return storeManager;
 };
