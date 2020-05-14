1ee3cdd98412d72d94d63e3e5148e4feb62435fa
svolkov <svolkov@everpoint.ru>
2020-05-14 09:40:06 +0300

    feat: rewrite undoable to reffect store with couple effects (undo, redo)

--- packages/undoable/src/index.ts
+++ packages/undoable/src/index.ts
@@ -1,60 +1,68 @@
-import { StoreType, manage } from "@reffect/core";
+import { StoreType, manage, store, effect, StoreMiddleware } from "@reffect/core";
 
-type Undoable = {
+type Undoable<Store extends StoreType> = {
+  history: StoreHistory<Store>;
   undo: VoidFunction;
   redo: VoidFunction;
 };
 
-export const undoable = <Store extends StoreType>(store: Store, undoSize: number = 10): Undoable => {
-  const storeManager = manage(store);
+export type StoreHistory<Store extends StoreType> = {
+  past: Store[];
+  present: Store;
+  future: Store[];
+};
 
-  const past: Store[] = [];
-  let present: Store = storeManager.initialState as Store;
-  const future: Store[] = [];
+export const undoable = <Store extends StoreType>(
+  storeRef: Store,
+  middlewares?: StoreMiddleware<StoreHistory<Store>>[],
+  size = 15,
+): Undoable<Store> => {
+  let updateFromUndoable = false;
+  const storeRefManager = manage(storeRef);
+  const storeHistory = store<StoreHistory<Store>>(
+    {
+      past: [],
+      present: storeRefManager.initialState as Store,
+      future: [],
+    },
+    `${storeRefManager.name}-history`,
+    middlewares,
+  );
 
-  let updateFromLib = false;
+  const undoRedo = (isRedo?: boolean): Partial<StoreHistory<Store>> | void => {
+    const historyArray = storeHistory[isRedo ? "future" : "past"];
+    if (historyArray.length) {
+      const sliced = historyArray.slice();
+      const present = sliced[isRedo ? "shift" : "pop"]();
+      if (present) {
+        updateFromUndoable = true;
+        storeRefManager.partialUpdate(present);
+        return {
+          present,
+          future: isRedo ? sliced : [storeHistory.present, ...storeHistory.future],
+          past: isRedo ? [...storeHistory.past, storeHistory.present] : sliced,
+        };
+      }
+    }
+  };
 
-  storeManager.subscribe((partialUpdate, prevState, currState) => {
-    if (updateFromLib) {
-      updateFromLib = false;
+  storeRefManager.subscribe((partialUpdate, prevState, currState) => {
+    if (updateFromUndoable) {
+      updateFromUndoable = false;
     } else {
-      if (future.length) {
-        future.splice(0, future.length);
-      }
-      if (past.length >= undoSize) {
-        past.shift();
-      }
-      past.push(prevState);
-      present = currState;
+      const pastLength = storeHistory.past.length;
+
+      manage(storeHistory).partialUpdate({
+        present: currState,
+        future: storeHistory.future.length ? [] : storeHistory.future,
+        past: [...storeHistory.past.slice(+(pastLength >= size), pastLength), prevState],
+      });
     }
   });
 
-  const undo = () => {
-    if (past.length) {
-      const lastPast = past.pop();
-      if (lastPast) {
-        future.unshift(present);
-        present = lastPast;
-        updateFromLib = true;
-        storeManager.partialUpdate(present);
-      }
-    }
-  };
-
-  const redo = () => {
-    if (future.length) {
-      const firstFuture = future.shift();
-      if (firstFuture) {
-        past.push(present);
-        present = firstFuture;
-        updateFromLib = true;
-        storeManager.partialUpdate(present);
-      }
-    }
-  };
-
   return {
-    undo,
-    redo,
+    undo: effect(storeHistory, () => undoRedo()),
+    redo: effect(storeHistory, () => undoRedo(true)),
+    history: storeHistory,
   };
 };
